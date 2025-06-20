9b6dfb3b2942331bf17aa341ea8bc6f3f05d1ec4
SquareShaper <anonymoushacker.cheaphacks@gmail.com>
2025-06-20 11:39:53 +0200

    Initial commit

---
+++ src/main/java/net/squareshaper/slacrystaltrees/SLACrystalTreesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.squareshaper.slacrystaltrees;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SLACrystalTreesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
